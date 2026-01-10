f3614a4351bd38c208ebf43b0ffae954afe71b63
jmgrgo <jmgranadosgo@gmail.com>
2026-01-10 01:10:26 -0500

    chore: initial commit

---
+++ src/main/java/org/jmgrgo/taskappserver/TaskAppServerApplication.java
@@ -0,0 +1,13 @@
+package org.jmgrgo.taskappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskAppServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jmgrgo/taskappserver/TaskAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jmgrgo.taskappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
