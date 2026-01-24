5635bb9b79331bda52438fe91c2a9e985700495c
jmgrgo <jmgranadosgo@gmail.com>
2026-01-24 15:50:26 -0500

    chore: initial commit

---
+++ src/test/java/org/jmgrgo/taskapp/TaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jmgrgo.taskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
